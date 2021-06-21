04d019a05a2c846a5a47013acc15e556d2ba3baa
UnlimitedHugs <unlimited.hugs@gmail.com>
2021-06-21 04:21:23 +0300

    Added Bevy

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,17 @@
-fn main() {
+use bevy::{
+    prelude::*,
+    app::App
+};
+use bevy_miniquad::{DrawFn, MiniquadPlugin};
+use std::sync::Arc;
+
+pub fn main() {
+    App::build()
+        .add_plugins(DefaultPlugins)
+        .insert_resource::<DrawFn>(Arc::new(Box::new(draw)))
+        .add_plugin(MiniquadPlugin)
+        .run();
+}
+
+fn draw(_app: &mut App) {
 }
