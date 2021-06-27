98c9fcadd6e999a6a91413ef32c2beff995f339f
UnlimitedHugs <unlimited.hugs@gmail.com>
2021-06-27 10:13:33 +0300

    Added cubes demo

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
-mod rendering;
+mod cubes_demo;
 mod quads_demo;
+mod rendering;
 mod shape;
 
-use bevy::{app::App, prelude::*};
+use bevy::{
+	app::{App, AppExit},
+	input::keyboard::KeyboardInput,
+	prelude::*,
+};
 use bevy_miniquad::MiniquadPlugin;
 use rendering::RenderingPlugin;
 
@@ -11,6 +16,16 @@ pub fn main() {
 		.add_plugins(DefaultPlugins)
 		.add_plugin(MiniquadPlugin)
 		.add_plugin(RenderingPlugin)
-		.add_plugin(quads_demo::QuadsDemoPlugin)
+		//.add_plugin(quads_demo::QuadsDemoPlugin)
+		.add_plugin(cubes_demo::CubesDemoPlugin)
+		.add_system_to_stage(CoreStage::Last, quit_on_esc.system())
 		.run();
+}
+
+fn quit_on_esc(mut input: EventReader<KeyboardInput>, mut exit: EventWriter<AppExit>) {
+	for evt in input.iter() {
+		if let Some(KeyCode::Escape) = evt.key_code {
+			exit.send(AppExit {});
+		}
+	}
 }
