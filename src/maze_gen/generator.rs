ea7be56742efabd59e8a93e1d3fa3e3ffc2ee490
UnlimitedHugs <unlimited.hugs@gmail.com>
2021-07-09 13:00:21 +0300

    Switched maze algorithm

--- src/maze_gen/generator.rs
+++ src/maze_gen/generator.rs
@@ -1,50 +1,64 @@
-use super::{grid_maze::WorldDirections, GridMaze, GridNode};
-use rand::seq::SliceRandom;
-use rand::{thread_rng, Rng};
+use super::{GridMaze, GridNode};
+use rand::{thread_rng, seq::SliceRandom};
 
-/// Generates a random maze using the Sidewinder algorithm. It's similar to binary tree but
-/// does have some differences. In a nutshell, it goes like this:
-///  1. Work through the maze row-wise, starting with the cell at 0,0. Initialize the “run” set to be empty.
-///  2. Add the current cell to the “run” set.
-///  3. For the current cell, randomly decide whether to carve east or not.
-///  4. If a passage was carved, make the new cell the current cell and repeat steps 2-4.
-///  5. If a passage was not carved, choose any one of the cells in the run set and carve a
-///     passage north. Then empty the run set, set the next cell in the row to be the current
-///     cell, and repeat steps 2-5.
-///  6. Continue until all rows have been processed.
+/// Generates a random maze using Wilson's algorithm:
+/// Like Aldous-Broder, this algorithm depends on the idea of a random walk, but with a twist.
+/// It performs what is called a loop-erased random walk, which means that as it goes, if the path
+/// it is forming happens to intersect with itself and form a loop, it erases that loop before
+/// continuing on.
+///
+/// 1. choose a point on the maze and mark it visited.
+/// 2. choose any unvisited node in the maze and perform a loop-erased random walk until you
+///    reach a visited node.
+/// 3. link all the nodes in the current random walk to the visited node
+/// 4. repeat step 2 until all nodes in the maze have been visited
 pub fn generate(height: usize, width: usize) -> GridMaze {
-	let at_eastern_boundary = |maze: &GridMaze, cell: &GridNode| maze.east(cell).is_none();
-	let at_northern_boundary = |maze: &GridMaze, cell: &GridNode| maze.north(cell).is_none();
-	// should we close out the current run of cells
-	let should_close_out = |maze: &GridMaze, cell: &GridNode| {
-		at_eastern_boundary(maze, cell)
-			|| (!at_northern_boundary(maze, cell) && thread_rng().gen::<bool>())
-	};
+    let mut maze = GridMaze::new(height, width);
 
-	let mut maze = GridMaze::new(height, width);
+    // choose a random node in the maze, this will be the first visited node
+    let first = maze.random_node();
+    // initialize unvisited to contain all positions in the maze except for first
+    let mut unvisited_nodes: Vec<GridNode> = maze
+        .iter_nodes()
+        .filter(|&node| *node != first)
+        .copied()
+        .collect();
 
-	for cur_index in 0..maze.len() {
-		let cur_node = maze[cur_index];
-		let mut runs = vec![cur_node];
+    // repeat until all nodes have been visited
+    while !unvisited_nodes.is_empty() {
+        // choose a random, unvisited node and add it to the `path` that is about to be walked
+        let mut cur_node = *unvisited_nodes.choose(&mut thread_rng()).unwrap();
+        // path contains the randomly walked nodes
+        let mut path: Vec<GridNode> = vec![cur_node];
 
-		if should_close_out(&maze, &cur_node) {
-			let rand_member = runs.choose(&mut thread_rng());
+        // while the cur_node is a member of unvisited nodes
+        while unvisited_nodes.contains(&cur_node) {
+            // choose a random neighbor of the current node
+            cur_node = *maze
+                .neighbors(&cur_node)
+                .choose(&mut thread_rng())
+                .expect("all nodes will have at least two neighbors");
 
-			// if the random_member has a north neighbor, carve a passage from the random cell
-			// to it's north neighbor
-			if let Some(rand_node) = rand_member {
-				if let Some(north_pos) = maze.north(rand_node) {
-					maze.link(&rand_node, &north_pos, true);
-				}
-			}
-			runs.clear();
-		} else {
-			// carve a passage from current cell to the east neighbor
-			if let Some(east_node) = maze.east(&cur_node) {
-				maze.link(&cur_node, &east_node, true);
-			}
-		}
-	}
+            // if the random neighbor is already in path, there is a loop, so remove it
+            if let Some(node_index) = path.iter().position(|node| *node == cur_node) {
+                path = path[0..=node_index].to_vec();
+            } else {
+                // the random neigbor is not going to make a loop, so push it onto the path
+                path.push(cur_node);
+            }
+        }
 
-	maze
+        // carve passages (i.e. link) between the nodes in path
+        let mut window = path.windows(2);
+        while let Some([node1, node2]) = window.next() {
+            maze.link(node1, node2, true);
+
+            // remove the nodes in the path from the vector of unvisited nodes
+            if let Some(path_idx) = unvisited_nodes.iter().position(|node| *node == *node1) {
+                unvisited_nodes.remove(path_idx);
+            }
+        }
+    }
+
+    maze
 }
