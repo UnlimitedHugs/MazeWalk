8c3fa8d06b66dd352f89e429f913aaa97f199286
UnlimitedHugs <unlimited.hugs@gmail.com>
2021-07-27 09:35:02 +0300

    Reformat

--- assets/shader.glsl
+++ assets/shader.glsl
@@ -14,12 +14,12 @@ uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
-void main(){
-	FragPos=vec3(model*vec4(pos,1.));
-	Normal=mat3(transpose(inverse(model)))*normal;
-	TexCoords=uv;
-	
-	gl_Position=projection*view*vec4(FragPos,1.);
+void main() {
+	FragPos = vec3(model * vec4(pos, 1.));
+	Normal = mat3(transpose(inverse(model))) * normal;
+	TexCoords = uv;
+
+	gl_Position = projection * view * vec4(FragPos, 1.);
 }
 
 #else // fragment shader
@@ -40,47 +40,47 @@ uniform float shininess;
 uniform sampler2D diffuse_tex;
 uniform sampler2D normal_tex;
 
-vec3 ambient_color=vec3(1.);
-vec3 normal_map_flat_color=vec3(.5,.5,1.);
+vec3 ambient_color = vec3(1.);
+vec3 normal_map_flat_color = vec3(.5, .5, 1.);
 
-mat3 cotangent_frame(vec3 normal,vec3 pos,vec2 uv){
-	vec3 dp1=dFdx(pos);
-	vec3 dp2=dFdy(pos);
-	vec2 duv1=dFdx(uv);
-	vec2 duv2=dFdy(uv);
-	
-	vec3 dp2perp=cross(dp2,normal);
-	vec3 dp1perp=cross(normal,dp1);
-	vec3 T=dp2perp*duv1.x+dp1perp*duv2.x;
-	vec3 B=dp2perp*duv1.y+dp1perp*duv2.y;
-	
-	float invmax=inversesqrt(max(dot(T,T),dot(B,B)));
-	return mat3(T*invmax,B*invmax,normal);
+mat3 cotangent_frame(vec3 normal, vec3 pos, vec2 uv) {
+	vec3 dp1 = dFdx(pos);
+	vec3 dp2 = dFdy(pos);
+	vec2 duv1 = dFdx(uv);
+	vec2 duv2 = dFdy(uv);
+
+	vec3 dp2perp = cross(dp2, normal);
+	vec3 dp1perp = cross(normal, dp1);
+	vec3 T = dp2perp * duv1.x + dp1perp * duv2.x;
+	vec3 B = dp2perp * duv1.y + dp1perp * duv2.y;
+
+	float invmax = inversesqrt(max(dot(T, T), dot(B, B)));
+	return mat3(T * invmax, B * invmax, normal);
 }
 
-void main(){
+void main() {
 	// normal
-	vec3 normal_sample=texture(normal_tex,TexCoords).rgb;
-	normal_sample=mix(normal_map_flat_color,normal_sample,normal_map_intensity);
-	mat3 tbn=cotangent_frame(Normal,FragPos,TexCoords);
-	vec3 norm=normalize(tbn*(normal_sample*2.-1.));
-	
+	vec3 normal_sample = texture(normal_tex, TexCoords).rgb;
+	normal_sample = mix(normal_map_flat_color, normal_sample, normal_map_intensity);
+	mat3 tbn = cotangent_frame(Normal, FragPos, TexCoords);
+	vec3 norm = normalize(tbn * (normal_sample * 2. - 1.));
+
 	// diffuse
-	vec3 light_dir=normalize(light_pos-FragPos);
-	float diff=max(dot(norm,light_dir),0.);
-	vec3 diffuse=diff*light_color*(texture(diffuse_tex,TexCoords).rgb*object_color);
-	
+	vec3 light_dir = normalize(light_pos - FragPos);
+	float diff = max(dot(norm, light_dir), 0.);
+	vec3 diffuse = diff * light_color * (texture(diffuse_tex, TexCoords).rgb * object_color);
+
 	// specular
-	vec3 view_dir=normalize(view_pos-FragPos);
-	vec3 reflect_dir=reflect(-light_dir,norm);
-	float spec=pow(max(dot(view_dir,reflect_dir),0.),shininess);
-	vec3 specular=specular_strength*spec*light_color;
+	vec3 view_dir = normalize(view_pos - FragPos);
+	vec3 reflect_dir = reflect(-light_dir, norm);
+	float spec = pow(max(dot(view_dir, reflect_dir), 0.), shininess);
+	vec3 specular = specular_strength * spec * light_color;
 
 	// ambient
 	vec3 ambient = ambient_intensity * ambient_color;
 
-	vec3 result=ambient+diffuse+specular;
-	FragColor=vec4(result,1.);
+	vec3 result = ambient + diffuse + specular;
+	FragColor = vec4(result, 1.);
 }
 
 #endif
