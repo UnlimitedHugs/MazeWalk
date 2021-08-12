8c42bf40a22fe54bb4872f4b5b496e096bb52f82
UnlimitedHugs <unlimited.hugs@gmail.com>
2021-08-12 10:49:31 +0400

    Entity parenting utils

---
+++ src/utils/children.rs
@@ -0,0 +1,39 @@
+use bevy_ecs::{prelude::{Entity, World}, system::{Command, EntityCommands}};
+
+pub struct Children(pub Vec<Entity>);
+
+pub fn despawn_with_children_recursive(world: &mut World, entity: Entity) {
+    if let Some(mut children) = world.get_mut::<Children>(entity) {
+        for e in std::mem::take(&mut children.0) {
+            despawn_with_children_recursive(world, e);
+        }
+    }
+
+    if !world.despawn(entity) {
+        eprintln!("Failed to despawn entity {:?}", entity);
+    }
+}
+
+#[derive(Debug)]
+pub struct DespawnRecursive {
+    entity: Entity,
+}
+
+impl Command for DespawnRecursive {
+    fn write(self: Box<Self>, world: &mut World) {
+        despawn_with_children_recursive(world, self.entity);
+    }
+}
+
+pub trait DespawnRecursiveExt {
+    /// Despawns the provided entity and its children.
+    fn despawn_recursive(&mut self);
+}
+
+impl<'a, 'b> DespawnRecursiveExt for EntityCommands<'a, 'b> {
+    /// Despawns the provided entity and its children.
+    fn despawn_recursive(&mut self) {
+        let entity = self.id();
+        self.commands().add(DespawnRecursive { entity });
+    }
+}
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,7 +1,11 @@
 mod color;
 mod shape;
 mod transform;
+mod rect;
+mod children;
 
 pub use color::Color;
 pub use shape::*;
 pub use transform::*;
+pub use rect::*;
+pub use children::*;
